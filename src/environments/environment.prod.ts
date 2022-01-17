ab99ed0934e0b15c6ca5cf834007986ef670d071
HwyToHell <holger.grzonka@web.de>
2022-01-17 11:56:49 +0100

    hosting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAmsO3Ax4RA-755vMkYB1_L1cjQJ-jnI8o",
+    authDomain: "who-has-tampered-with-my-car.firebaseapp.com",
+    databaseURL: "https://who-has-tampered-with-my-car-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "who-has-tampered-with-my-car",
+    storageBucket: "who-has-tampered-with-my-car.appspot.com",
+    messagingSenderId: "359366277205",
+    appId: "1:359366277205:web:272e85d63f14d5c9b6bcdc",
+    measurementId: "G-W3R7WSGPJM"
+  }
 };
